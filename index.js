2bc4126ea82db01e946402af9455824d0922636f
Keenen Harral <kharral23@yahoo.com>
2022-11-16 21:13:37 -0800

    commit tree 260c61a71ea926e80079073ce59c434b4326f1ee

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(newArray) {
+    const drivers = newArray.slice(0, 2);
+    return drivers;
+
+}
+
+const returnLastTwoDrivers = function(newArray2) {
+    const drivers2 = newArray2.slice(2, 4);
+    return drivers2;
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers(element),
+    returnLastTwoDrivers(element),
+];
